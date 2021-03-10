6b6a73e7e13b69d734fd348cf0caf2b7c4a49c20
Jeremías <jeremias@talkingpts.org>
2021-03-09 21:28:13 -0300

    api run helper script

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 set -eu
 UWS_LOG=${UWS_LOG:-'default'}
+BOT=${1:?'bot name?'}
+SCRIPT=${2:-''}
+ARGS=''
+if test 'X' != "X${SCRIPT}"; then
+	ARGS="-run ${SCRIPT}"
+fi
 exec docker run -it --rm --name uws-bot-devel \
 	--hostname bot-devel.uws.local \
 	-e UWSENV=bot/staging \
 	-e "UWS_LOG=${UWS_LOG}" \
 	-v "${PWD}:/uws/share/uwsbot:ro" \
 	-v "${PWD}/docker/auth:/home/uws/.config/uws/bot:ro" \
-	-u uws uws/uwsbot-devel "$@"
+	-u uws uws/uwsbot-devel /uws/bin/uwsbot -env staging -name ${BOT} ${ARGS}
